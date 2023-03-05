a05c66a038f9cd827c59e3e7c3c3abbbd3a6d37b
nhayenquynh <nhayenquynh@gmail.com>
2023-03-04 19:46:17 -0800

    update

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import plotly_express as px
 
 st.title("Sprint 4 Project: Analyzing Car Advertising Dataset")
 
+st.markdown('Introduction: This project is to analyze a dataset of cars that is collected from 1970s-2010s. I will analyze the most common types of vehicles, how much revenue each year made, and the correlation between the numbers of cyclinder and cost.')
+
 df =pd.read_csv('/Users/quynhnguyen/Desktop/sprint_4_project/vehicles_us.csv')
 
 st.write(df.head(5))
@@ -30,3 +32,5 @@ cylinder = df.groupby('cylinders').agg({'price':'mean'}).reset_index()
 cylinder1 = cylinder.drop(index=1)
 st.write(px.scatter(cylinder1, x='cylinders', y='price', color='cylinders'))
 st.markdown('There is an increased in price as the number of cyclinder increases. Except for 0 and 10 cyclinders, these can be considered as outliers.')
+
+st.markdown('Conclusion: Analysis of car prices, car sales, how long it took to sell a car, and cyclinders were done. From the first graph, we see that bus, pickup, and truck cost the most, on average. Whereas smaller cars cost less. From graph 2, we see that car sale produce thousands of revenues in 1900s to billions in 2000s. However, there was a drop in car sale in 2019. Another analysis that was made was to see the average days it took to sell cars with different conditions. It takes 39 days, on average, in every condition. Cyclinder cost increases as the number of cyclinder increases, with outlier for 0 and 10 cyclinders.')
